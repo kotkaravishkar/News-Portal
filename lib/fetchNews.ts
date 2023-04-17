4f413cbce81eb9989d7ccf1cd4fa3542882a09c2
Avishkar Kotkar <kotkaravi07@gmail.com>
2023-04-18 02:51:17 +0530

    Curl

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -2,13 +2,15 @@ import { categories } from "@/constants"
 const fetchNews = async ( category?: Category | string, keywords?: string, isDynamic?:boolean) => {
 
  // GraphQL Query 
- 
  // Fetch Function with Nextjs 13 API Routes 
-
-
  // Sort function by images vs not images present
+ // return response
 
- // 
 }
 
 export default fetchNews;
+
+
+// Example Import
+
+ // stepzen import curl "http://api.mediastack.com/v1/news?access_key="
